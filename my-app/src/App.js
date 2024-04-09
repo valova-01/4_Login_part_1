58f88d8c0fa8eadd5e76b57c334e75131dd6f772
anna <anna.valova.01@mail.ru>
2024-04-10 01:26:51 +0300

    login_work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-import { createElement } from 'react';
+import { useState, useRef } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit ', createElement('code', null, 'src/App.js'), ' and save to reload.'),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-		),
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
 
-		createElement('footer', null, createElement('p', null, 'Текущий год: ', currentYear)),
+	const [formErrors, setFormErrors] = useState({
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+
+	const registerButtonRef = useRef(null);
+
+	const validateEmail = (email) => /\S+@\S+\.\S+/.test(email) || email === '';
+	const validatePassword = (password) => (password.length >= 3 && password.length <= 20) || password === '';
+
+	const validateField = (fieldName, value) => {
+		let error = '';
+
+		if (fieldName === 'email') error = validateEmail(value) ? '' : 'Некорректный email';
+		else if (fieldName === 'password') error = validatePassword(value) ? '' : 'Пароль должен содержать от 3 до 20 символов';
+		else if (fieldName === 'confirmPassword') error = value === formData.password ? '' : 'Пароли не совпадают';
+
+		setFormErrors({ ...formErrors, [fieldName]: error });
+	};
+
+	const handleInputChange = (fieldName, value) => {
+		setFormData({ ...formData, [fieldName]: value });
+		validateField(fieldName, value);
+		if ((fieldName === 'password' || fieldName === 'confirmPassword') && formData.password === value)
+			setFormErrors({ ...formErrors, confirmPassword: '' });
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (!Object.values(formErrors).every((error) => error === '') || Object.values(formData).some((value) => value === '')) {
+			console.log('Форма содержит ошибки. Пожалуйста, исправьте их.');
+			return;
+		}
+		console.log('Данные формы:', formData);
+	};
+
+	const focusRegisterButton = () => {
+		if (Object.values(formErrors).every((error) => error === '') && Object.values(formData).every((value) => value !== '')) {
+			registerButtonRef.current.focus();
+		}
+	};
+
+	const isFormValid = () =>
+		Object.values(formErrors).every((error) => error === '') &&
+		formData.email !== '' &&
+		formData.password !== '' &&
+		formData.confirmPassword !== '';
+
+	focusRegisterButton();
+
+	return (
+		<div className={styles.app}>
+			<h1>Регистрация</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label>Email:</label>
+					<input type="email" value={formData.email} onChange={(event) => handleInputChange('email', event.target.value)} />
+					<span>{!validateEmail(formData.email) ? 'Некорректный email' : formErrors.email}</span>
+				</div>
+				<div>
+					<label>Придумайте пароль:</label>
+					<input type="password" value={formData.password} onChange={(event) => handleInputChange('password', event.target.value)} />
+					<span>{!validatePassword(formData.password) ? 'Пароль должен содержать от 3 до 20 символов' : formErrors.password}</span>
+				</div>
+				<div>
+					<label>Повторите пароль:</label>
+					<input
+						type="password"
+						value={formData.confirmPassword}
+						onChange={(event) => handleInputChange('confirmPassword', event.target.value)}
+					/>
+					<span>{formErrors.confirmPassword}</span>
+				</div>
+				<button type="submit" disabled={!isFormValid()}>
+					Зарегистрироваться
+				</button>
+				<button ref={registerButtonRef} style={{ display: 'none' }}></button>
+			</form>
+		</div>
 	);
 };
